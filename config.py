f003b77b81396b588f1183b9fac0636a96319f76
soham6540 <savitafarkade6540@gmail.com>
2024-05-07 09:21:19 +0530

    Update config.py
    Signed-off-by: soham6540 <savitafarkade6540@gmail.com>

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID"))
+API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
 
 # Get your token from @BotFather on Telegram.
@@ -19,10 +19,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002036001760))
+LOGGER_ID = getenv("LOGGER_ID", -1002036001760)
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = getenv("OWNER_ID", 1356469075)
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
